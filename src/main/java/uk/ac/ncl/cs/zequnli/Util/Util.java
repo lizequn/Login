eadff2feb79c2f33d884672997ed92f43d19bcd3
lizequn <lzq910123@gmail.com>
2013-12-03 17:09:22 +0000

    login&register finish

---
+++ src/main/java/uk/ac/ncl/cs/zequnli/Util/Util.java
@@ -0,0 +1,18 @@
+package uk.ac.ncl.cs.zequnli.Util;
+
+/**
+ * @Auther: Li Zequn
+ * Date: 03/12/13
+ */
+public class Util {
+    private final String clientId = "223283651290.apps.googleusercontent.com";
+    private final String clientSecret = "lp3SDksMkQ0Vd40NUBjqXpkO";
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+}
