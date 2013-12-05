386ed02816fedd3a3884339160196ffb938a62be
lizequn <lzq910123@gmail.com>
2013-12-05 18:10:22 +0000

    add provided scope for tomcat

---
+++ src/main/java/uk/ac/ncl/cs/zequnli/controller/OAuthController.java
@@ -0,0 +1,12 @@
+package uk.ac.ncl.cs.zequnli.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Auther: Li Zequn
+ * Date: 05/12/13
+ */
+@Controller
+public class OAuthController {
+
+}
